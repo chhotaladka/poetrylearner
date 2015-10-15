4498cb0caf1803bb2b7d687776c632504185ac2e
chhotaladka <chhotaladka02@gmail.com>
2015-10-15 23:44:51 +0530

    new methods added

--- crawlers/models.py
+++ crawlers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
 from django.contrib import auth
+from django.core.urlresolvers import reverse
+import json
 from snippets.models.snippet import Snippet
 
 # Create your models here.
@@ -16,6 +18,9 @@ class RawArticle(models.Model):
     valid = models.BooleanField(default=False)
     snippet = models.ForeignKey(Snippet, related_name='ref_articles', null=True, blank=True, verbose_name="related entry in article table")
     
+    class Meta:
+        ordering = ['-added_at']
+             
 
     def __str__(self):          # on Python 3
         return self.source_url
@@ -24,8 +29,26 @@ class RawArticle(models.Model):
         return self.source_url
       
     def get_absolute_url(self):
-        pass
+        kwargs = {'pk': str(self.id)}
+        return reverse('crawlers:article-details', kwargs=kwargs)
     
+    def get_source_url(self):
+        return self.source_url
+    
+    def get_validity(self):
+        return self.valid
+           
+    def get_title(self):       
+        # return title extracted from self.content
+        return json.loads(self.content)['title']
+    
+    def get_author(self):
+        # return author extracted from self.content
+        return json.loads(self.content)['author']
+    
+    def get_poem(self):
+        # return poem extracted from self.content
+        return json.loads(self.content)['poem']
     
     def save(self, *args, **kwargs):
         print "Model RawArticle save called"

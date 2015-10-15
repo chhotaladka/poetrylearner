66499e0dcc8491c04569ccdf84727b524e900b68
chhotaladka <chhotaladka02@gmail.com>
2015-10-15 23:51:32 +0530

    raw articles list view added

--- crawlers/urls.py
+++ crawlers/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     
-    url(r'^article/(?P<pk>\d+)$', views.RawArticleDetails, name='article-details'),
+    url(r'^article/(?P<pk>\d+)$', views.raw_article_details, name='article-details'),
+    url(r'^article/?$', views.raw_article_list, name='article-list'),
     
 ]
--- crawlers/views.py
+++ crawlers/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import *
+import json
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from crawlers.models import RawArticle
 
 # Create your views here.
@@ -7,10 +9,47 @@ from crawlers.models import RawArticle
 """
 Details of the RawArticle
 """
-def RawArticleDetails(request, pk):
-    a = get_object_or_404(RawArticle, pk=pk)
+def raw_article_details(request, pk):
+    obj = get_object_or_404(RawArticle, pk=pk)
+    
+    data = json.loads(obj.content)
+    # data contains ['index', 'title', 'author', 'poem', 'url']
         
-    context = {'article': a}
+    context = {'article': obj, 'data': data}
     template = "crawlers/article-details.html"
+
+    return render(request, template, context)
+
+
+def raw_article_list(request):
+    """
+    
+    """    
+    
+    # Get the object list           
+    obj_list = RawArticle.objects.all()
+    
+    ##
+    # Check the parameters passed in the URL and process accordingly
+
+    
+    ##
+    # Check for permissions and render the list of articles
+    
+    
+    # Pagination
+    paginator = Paginator(obj_list, 20) # Show 20 articles per page    
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        articles = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        articles = paginator.page(paginator.num_pages)
+            
+    context = {'articles': articles}
+    template = 'crawlers/article-list.html'    
 
     return render(request, template, context)

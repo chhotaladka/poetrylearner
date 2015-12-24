7d89a922ae13493db2afa147436968b34d38031e
chhotaladka <chhotaladka02@gmail.com>
2015-12-25 02:27:05 +0530

    search related methods added to common

---
+++ common/search.py
@@ -0,0 +1,58 @@
+"""
+To implement search related queries
+Source: http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap
+"""
+from django.db.models import Q
+import re
+import os, sys, traceback
+
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
+        and grouping quoted words together.
+        Example:
+        
+        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    
+    '''
+    try:
+        ret = [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+    except:
+        print ("Error: Unexpected error:", sys.exc_info()[0])
+        for frame in traceback.extract_tb(sys.exc_info()[2]):
+            fname,lineno,fn,text = frame
+            print ("DBG:: Error in %s on line %d" % (fname, lineno))
+        ret = []
+                
+    return ret 
+
+def get_query(query_string, search_fields):
+    ''' Returns a query, that is a combination of Q objects. That combination
+        aims to search keywords within a model by testing the given search fields.
+    
+    '''    
+    query = None # Query to search for every search term
+    
+    try:        
+        terms = normalize_query(query_string)
+        for term in terms:
+            or_query = None # Query to search for a given term in each field
+            for field_name in search_fields:
+                q = Q(**{"%s__icontains" % field_name: term})
+                if or_query is None:
+                    or_query = q
+                else:
+                    or_query = or_query | q
+            if query is None:
+                query = or_query
+            else:
+                query = query & or_query
+    except:
+        print ("Error: Unexpected error:", sys.exc_info()[0])
+        for frame in traceback.extract_tb(sys.exc_info()[2]):
+            fname,lineno,fn,text = frame
+            print ("DBG:: Error in %s on line %d" % (fname, lineno))            
+            
+    return query
--- projects/views/author.py
+++ projects/views/author.py
@@ -15,6 +15,7 @@ from django.template.loader import render_to_string
 from django.db.models import Q
 from django.utils.decorators import method_decorator
 import json
+from common.search import get_query
 
 # Create your views here.
 
@@ -220,37 +221,25 @@ def search_author(request):
     """
     For ajax search of author select field
     """
-    try:
-        q_objects = Q()
-        # Search query
-        q = request.GET.get('q', None)                  
+
+    # Search query
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+        entry_query = get_query(query_string, ['name_en', 'name', 'sobriquet',])
+        obj_list = Author.objects.filter(entry_query).order_by('name')[:5]
+    else:
+        obj_list = []
+            
+    result = []
+    for obj in obj_list:
+        data = {}
+        data['id'] = obj.id
+        data['name'] = obj.name
+        data['name_en'] = obj.name_en
+        data['birth'] = obj.date_birth.year if obj.date_birth else ''
+        data['death'] = obj.date_death.year if obj.date_death else ''
+        result.append(data)
         
-        # Apply Search query
-        if q:
-            q = q.strip()      
-            # get the authors with 'name'        
-            q_objects &= Q(name_en__icontains=q) | Q(name__icontains=q) | Q(sobriquet__icontains=q)      
-                   
-            obj_list = Author.objects.all().filter(q_objects).order_by('name')[:5]
-        else:
-            obj_list = []
-                
-        result = []
-        for obj in obj_list:
-            data = {}
-            data['id'] = obj.id
-            data['name'] = obj.name
-            data['name_en'] = obj.name_en
-            data['birth'] = obj.date_birth.year if obj.date_birth else ''
-            data['death'] = obj.date_death.year if obj.date_death else ''
-            result.append(data)
-            
-        r = json.dumps(result)
-        print r    
-    except:
-        print ("Error: Unexpected error:", sys.exc_info()[0])
-        for frame in traceback.extract_tb(sys.exc_info()[2]):
-            fname,lineno,fn,text = frame
-            print ("DBG:: Error in %s on line %d" % (fname, lineno))
-            
+    r = json.dumps(result)
+                  
     return HttpResponse(r, content_type="application/json")    

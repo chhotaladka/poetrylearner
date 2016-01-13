84f38ca69a97c9b22172853adbae53514d4170fc
chhotaladka <chhotaladka02@gmail.com>
2016-01-14 00:28:18 +0530

    admin updated

--- repository/admin.py
+++ repository/admin.py
@@ -4,31 +4,81 @@ from repository.models import *
 # Register your models here.
 
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description', 'same_as',
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'description']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description', 'has_map', 'address',
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'description', 'address']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description', 'manufacturer',
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'description']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class OrganizationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description', 'type',
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'description']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class EventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description', 
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'description']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'description', 'isbn', 'creator', 'publisher',
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'description', 'isbn']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class PoetryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'creator', 'body', 'date_published', 
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'body']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class SnippetAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'creator', 'same_as', 'date_published', 
+                    'added_by', 'date_added', 
+                    'modified_by', 'date_modified')
+    
+    search_fields = ['name', 'body']
+    list_filter = ['date_added', 'date_modified']
+    
 
 class ReferenceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'body')
+    
+    search_fields = ['name','body']
 
 
 admin.site.register(Person, PersonAdmin)

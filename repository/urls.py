7048fa626682ca67762e31ca47b5cd398eb1c27e
chhotaladka <chhotaladka02@gmail.com>
2016-02-04 23:59:58 +0530

    title slug is just for SEO purpose, it can be anything ascii, unicode, spaces etc.

--- repository/urls.py
+++ repository/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     url(r'^add/(?P<type>[\w.@+-]+)/(?P<pk>\d+)?/?$', views.writers.AddItem.as_view(), name='add-item'),
     url(r'^add/?$', views.writers.add, name='add'),
     
-    url(r'^data/(?P<type>[\w.@+-]+)/(?P<pk>\d+)/(?P<slug>[\w.@+-]+)?/publish/?$', views.writers.publish, name='publish'),
+    url(r'^data/(?P<type>[\w.@+-]+)/(?P<pk>\d+)/(?P<slug>.+)?/publish/?$', views.writers.publish, name='publish'),
     
-    url(r'^data/(?P<type>[\w.@+-]+)/(?P<pk>\d+)/(?P<slug>[\w.@+-]+)?/?$', views.readers.item, name='item'),
+    url(r'^data/(?P<type>[\w.@+-]+)/(?P<pk>\d+)/(?P<slug>.+)?/?$', views.readers.item, name='item'),
     url(r'^data/(?P<type>[\w.@+-]+)/?$', views.readers.list, name='list'),
     url(r'^data/?$', views.readers.items, name='view'),
     

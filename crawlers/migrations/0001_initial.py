a1b0301f444c1c553613bf2c60f80d9fb96899dc
chhotaladka <chhotaladka02@gmail.com>
2016-02-28 21:40:32 +0530

    migrations deleted

--- crawlers/migrations/0001_initial.py
+++ crawlers/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('snippets', '0001_initial'),
     ]
 
     operations = [
@@ -16,10 +15,29 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('source_url', models.URLField(max_length=1000)),
+                ('title', models.CharField(max_length=200, null=True, blank=True)),
+                ('author', models.CharField(max_length=100, null=True, blank=True)),
                 ('content', models.TextField()),
                 ('added_at', models.DateTimeField(auto_now_add=True)),
                 ('valid', models.BooleanField(default=False)),
-                ('snippet', models.ForeignKey(related_name='ref_articles', verbose_name=b'related entry in article table', blank=True, to='snippets.Snippet', null=True)),
             ],
+            options={
+                'ordering': ['-added_at'],
+            },
+        ),
+        migrations.CreateModel(
+            name='RawAuthor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('source_url', models.URLField(max_length=1000)),
+                ('name', models.CharField(max_length=200)),
+                ('birth', models.CharField(max_length=100, null=True, blank=True)),
+                ('death', models.CharField(max_length=100, null=True, blank=True)),
+                ('added_at', models.DateTimeField(auto_now_add=True)),
+                ('valid', models.BooleanField(default=False)),
+            ],
+            options={
+                'ordering': ['-added_at'],
+            },
         ),
     ]

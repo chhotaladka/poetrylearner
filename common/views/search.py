da55244e5cf3b3d6fbfccc58af78db6da868e745
chhotaladka <chhotaladka02@gmail.com>
2016-02-28 22:11:34 +0530

    Dependency on Projects app removed

--- common/views/search.py
+++ common/views/search.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
 from django.template.context_processors import request
 from django.contrib import messages
-from projects import utils
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import JsonResponse
@@ -13,7 +12,6 @@ from django.db.models import Q
 from django.utils.decorators import method_decorator
 import json
 from common.search import get_query
-from projects.models import Author
 
 # Create your views here.
 
@@ -24,23 +22,23 @@ def site_search(request):
     """
     
     # Search query
-    if ('q' in request.GET) and request.GET['q'].strip():
-        query_string = request.GET['q']
-        entry_query = get_query(query_string, ['name_en', 'name', 'sobriquet',])
-        obj_list = Author.objects.filter(entry_query).order_by('name')[:5]
-    else:
-        obj_list = []
+#     if ('q' in request.GET) and request.GET['q'].strip():
+#         query_string = request.GET['q']
+#         entry_query = get_query(query_string, ['name_en', 'name', 'sobriquet',])
+#         obj_list = Author.objects.filter(entry_query).order_by('name')[:5]
+#     else:
+#         obj_list = []
             
     result = []
-    for obj in obj_list:
-        data = {}
-        data['id'] = obj.id
-        data['name'] = obj.name
-        data['name_en'] = obj.name_en
-        data['birth'] = obj.date_birth.year if obj.date_birth else ''
-        data['death'] = obj.date_death.year if obj.date_death else ''
-        data['url'] = obj.get_absolute_url()
-        result.append(data)
+#     for obj in obj_list:
+#         data = {}
+#         data['id'] = obj.id
+#         data['name'] = obj.name
+#         data['name_en'] = obj.name_en
+#         data['birth'] = obj.date_birth.year if obj.date_birth else ''
+#         data['death'] = obj.date_death.year if obj.date_death else ''
+#         data['url'] = obj.get_absolute_url()
+#         result.append(data)
         
     r = json.dumps(result)
                   
@@ -53,23 +51,9 @@ def site_advance_search(request):
     """
 
     # Search query
-    if ('q' in request.GET) and request.GET['q'].strip():
-        query_string = request.GET['q']
-        entry_query = get_query(query_string, ['name_en', 'name', 'sobriquet',])
-        obj_list = Author.objects.filter(entry_query).order_by('name')[:5]
-    else:
-        obj_list = []
+
             
     result = []
-    for obj in obj_list:
-        data = {}
-        data['id'] = obj.id
-        data['name'] = obj.name
-        data['name_en'] = obj.name_en
-        data['birth'] = obj.date_birth.year if obj.date_birth else ''
-        data['death'] = obj.date_death.year if obj.date_death else ''
-        data['url'] = obj.get_absolute_url()
-        result.append(data)
         
     r = json.dumps(result)
                   

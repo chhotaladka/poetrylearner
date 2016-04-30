1863b44563f4cf05882689c3e522d96f45918da0
chhotaladka <chhotaladka02@gmail.com>
2016-04-30 21:45:28 +0530

    decorator bug fixed: 403 if not has group

--- common/decorators.py
+++ common/decorators.py
@@ -1,4 +1,24 @@
-from django.contrib.auth.decorators import user_passes_test
+from functools import wraps
+from django.utils.decorators import available_attrs
+from django.core.exceptions import PermissionDenied
+
+
+def user_has_group(test_func):
+    """
+    Decorator for views that checks that the user passes the given test,
+    raise permission denied if necessary. The test should be a callable
+    that takes the user object and returns True if the user passes.
+    """
+
+    def decorator(view_func):
+        @wraps(view_func, assigned=available_attrs(view_func))
+        def _wrapped_view(request, *args, **kwargs):
+            if test_func(request.user):
+                return view_func(request, *args, **kwargs)
+            raise PermissionDenied
+        return _wrapped_view
+    return decorator
+
 
 def group_required(*group_names):
     """Requires user membership in at least one of the groups passed in."""
@@ -7,4 +27,6 @@ def group_required(*group_names):
             if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
                 return True
         return False
-    return user_passes_test(in_groups)
+    
+    return user_has_group(in_groups)
+        

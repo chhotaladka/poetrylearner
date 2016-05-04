46928dec732728c84efe3a23ec3c06466dff6986
chhotaladka <chhotaladka02@gmail.com>
2016-05-05 00:23:34 +0530

    dashboard app

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,250 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from allauth.socialaccount.models import SocialAccount
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    '''
+    User profile populated using social account from Facebook, Google and Twitter respectively.
+    '''
+    user = models.OneToOneField(User, related_name='profile')
+    date_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+    signup_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):          # on Python 3
+        return "{}'s profile".format(self.user.username)
+    
+    def __unicode__(self):      # on Python 2
+        return "{}'s profile".format(self.user.username)        
+ 
+    class Meta:
+        db_table = 'user_profile'
+    
+
+    def get_absolute_url(self):        
+        kwargs = {'user_id': self.user.id}
+        return reverse('dashboard:user-profile', kwargs=kwargs)
+        
+    def _get_social_account(self, provider=None):
+        '''
+        Return the social account object of Facebook, Google and Twitter in that order if provier is None else return the provider account.
+        '''
+        if provider != None:
+            account_uid = SocialAccount.objects.filter(user_id=self.user.id, provider=provider)
+            if len(account_uid):
+                return account_uid[0]
+            else:
+                return None
+            
+        try:
+            account_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+            if len(account_uid):
+                return account_uid[0]
+    
+            account_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='google')
+            if len(account_uid):
+                return account_uid[0]
+    
+            account_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='twitter')
+            if len(account_uid):
+                return account_uid[0]
+        except:
+            return None
+        else:
+            return None
+    
+    def is_social_account_exist(self, provider):
+            account_uid = SocialAccount.objects.filter(user_id=self.user.id, provider=provider)
+            if len(account_uid):
+                return True
+            else:
+                return False
+        
+    
+    def get_avatar_url(self, provider=None):
+        '''
+        If provider is None, return the avatar of social account Facebook, Google and Twitter in that order.
+        TODO: return default avatar if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            return profile.get_avatar_url()
+        else:
+            return None
+    
+    def get_username(self, provider=None):
+        '''
+        If provider is None, return the username of social account Facebook, Google and Twitter in that order.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            if self.get_provider_name(profile.provider) == 'facebook':
+                return self._get_fb_username(profile)
+            elif self.get_provider_name(profile.provider) == 'google':
+                return self._get_google_username(profile)
+            elif self.get_provider_name(profile.provider) == 'twitter':
+                return self._get_tw_username(profile)
+            else:
+                return self.user.username
+        else:
+            return self.user.username
+
+    def get_name(self, provider=None):
+        return "{0} {1}".format(self.get_first_name(provider), self.get_last_name(provider))
+
+    def get_first_name(self, provider=None):
+        '''
+        Return the first name from social account Facebook, Google and Twitter in that order, if provider is None.
+        TODO: return default first name from User model if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            if self.get_provider_name(profile.provider) == 'facebook':
+                return self._get_fb_fname(profile)
+            elif self.get_provider_name(profile.provider) == 'google':
+                return self._get_google_fname(profile)
+            elif self.get_provider_name(profile.provider) == 'twitter':
+                return self._get_tw_fname(profile)
+            else:
+                return self.user.first_name
+        else:
+            return self.user.first_name
+    
+    def get_last_name(self, provider=None):
+        '''
+        Return the last name from social account Facebook, Google and Twitter in that order, if provider is None.
+        TODO: return default last name from User model if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            if self.get_provider_name(profile.provider) == 'facebook':
+                return self._get_fb_lname(profile)
+            elif self.get_provider_name(profile.provider) == 'google':
+                return self._get_google_lname(profile)
+            elif self.get_provider_name(profile.provider) == 'twitter':
+                return self._get_tw_lname(profile)
+            else:
+                return self.user.last_name
+
+        else:
+            return self.user.last_name
+
+    def get_gender(self, provider=None):
+        '''
+        Return the gender from social account Facebook, Google and Twitter in that order, if provider is None.
+        TODO: return default last name from User model if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            if self.get_provider_name(profile.provider) == 'twitter':
+                return "---"
+            return profile.extra_data['gender']
+        else:
+            return "---"
+
+    def get_email(self, provider=None):
+        '''
+        Return the email of social account Facebook, Google and Twitter in that order, if provider is None.
+        TODO: return default email if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            if self.get_provider_name(profile.provider) == 'twitter':
+                return self.user.email
+            return profile.extra_data['email']
+        else:
+            return self.user.email
+
+    def get_social_url(self, provider=None):
+        '''
+        Return the social url of social account Facebook, Google and Twitter in that order, if provider is None.
+        todo: return default blank  if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            print profile.extra_data
+            if self.get_provider_name(profile.provider) == 'facebook':
+                return self._get_fb_link(profile)
+            elif self.get_provider_name(profile.provider) == 'google':
+                return self._get_google_link(profile)
+            elif self.get_provider_name(profile.provider) == 'twitter':
+                return self._get_tw_link(profile)
+            else:
+                return ""
+        else:
+            return ""
+       
+    def get_provider_name(self,provider=None):
+        '''
+        Return the provider name of social account Facebook, Google and Twitter in that order, if provider is None.
+        TODO: return default blank  if no social account is found.
+        '''
+        profile = self._get_social_account(provider)
+        if profile != None:
+            return profile.provider
+        else:
+            return provider    
+
+    def get_birthday(self):
+        return self.date_birth  
+    
+    def get_signup_time(self):
+        return self.signup_date
+
+    ##
+    # Private functions for retrieving the fname, lname, email etc. from GooGle, Facebook and twitter
+        
+    def _get_google_fname(self,profile):
+        return str(profile.extra_data['name']).split(' ')[0].encode('utf-8')
+    
+    def _get_google_lname(self,profile):
+        return profile.extra_data['family_name'].encode('utf-8')
+
+    def _get_google_email(self,profile):
+        return profile.extra_data['email'].encode('utf-8')
+
+    def _get_google_email(self,profile):
+        return profile.extra_data['email'].encode('utf-8')
+
+    def _get_google_username(self,profile):
+        return profile.extra_data['given_name'].encode('utf-8')
+
+    def _get_google_link(self,profile):
+        return profile.extra_data['link']
+
+    def _get_fb_fname(self,profile):
+        return profile.extra_data['first_name'].encode('utf-8')
+    
+    def _get_fb_lname(self,profile):
+        return profile.extra_data['last_name'].encode('utf-8')
+
+    def _get_fb_email(self,profile):
+        return profile.extra_data['email'].encode('utf-8')
+
+    def _get_fb_username(self,profile):
+        return profile.extra_data['name'].encode('utf-8')
+
+    def _get_fb_link(self,profile):
+        return profile.extra_data['link']
+    
+    def _get_tw_fname(self,profile):
+        return str(profile.extra_data['name']).split(' ')[0].encode('utf-8')
+
+    def _get_tw_lname(self,profile):
+        return str(profile.extra_data['name']).split(' ')[-1].encode('utf-8')
+
+    def _get_tw_username(self,profile):
+        return profile.extra_data['screen_name'].encode('utf-8')
+
+    def _get_tw_link(self,profile):
+        return  "//twitter.com/" + str(profile.extra_data['screen_name'])
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,58 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+import os, sys, traceback
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from allauth.socialaccount.models import SocialAccount
+
+from dashboard.models import UserProfile
 
 # Create your views here.
+
+@receiver(user_logged_in)
+def CreateProfile(sender, request, user,**kwargs):
+    '''
+    This function catches the signal for social login or social account add, 
+    and check for the User profile object: if exist then do nothing,
+    if not then create it and set the gender field.
+    '''
+    try:        
+        profile = UserProfile.objects.get(user=user)
+        print "DBG:: User profile exist, do nothing"
+    except UserProfile.DoesNotExist:
+        print "DBG:: User profile does not exist, create a new one"
+  
+        profile = UserProfile()
+        profile.user = user
+        try:
+            sociallogin = SocialAccount.objects.get(user=user)
+            print "DBG:: Caught the signal--> Printing extra data of the account: \n", sociallogin.extra_data
+            if('google' == sociallogin.provider ):
+                user.first_name = sociallogin.extra_data['given_name']
+                user.last_name = sociallogin.extra_data['family_name']
+                user.save()
+            elif ('facebook' == sociallogin.provider ):
+                user.first_name = sociallogin.extra_data['first_name']
+                user.last_name = sociallogin.extra_data['last_name']
+                user.save()
+            profile.gender = sociallogin.extra_data['gender']
+        except:
+            print ("Error:: Unexpected error:", sys.exc_info()[0])
+            for frame in traceback.extract_tb(sys.exc_info()[2]):
+                fname,lineno,fn,text = frame
+                print ("DBG:: Error in %s on line %d" % (fname, lineno))           
+            print "DBG:: Gender does not exist in social account"
+            
+        profile.save()
+
 
 @login_required
 def user_home(request, user_id):
     '''
     Home page of the user
-    '''
+    '''  
     
     ## Make the context and render     
     context = {}
@@ -22,31 +65,47 @@ def user_profile(request, user_id):
     '''
     Profile page of the user
     '''
+    print "user profile of ", user_id
     try:
-        user_id = int(user_id)
-        if request.user.is_authenticated():
-            if request.user.id == user_id:
-                scope = request.GET.get('viewas', None)
-                if scope == 'public':
-                    return public_profile(request, user_id)
-                else:
-                    return private_profile(request)
-
-        return public_profile(request, user_id)
-        
+        user_id = int(user_id) # required for comparision
     except ValueError:
         print "DBG:: invalid request for user_id ", user_id
         raise Http404
+            
+    if request.user.is_authenticated():
+        if request.user.id == user_id:
+            scope = request.GET.get('viewas', None)
+            if scope == 'public':
+                return public_profile(request, user_id)
+            else:
+                return private_profile(request)
 
+    return public_profile(request, user_id)
+        
 
 @login_required
 def private_profile(request):
     '''
     Private profile of the user
     '''
+    print "DBG:: private profile"
+    profile = UserProfile.objects.get(user=request.user.id)
 
+    social_accounts = []
+    providers = ["Facebook", "Google", "Twitter"]
+    for provider in providers:
+        if profile.is_social_account_exist(provider):
+            extra_context = {}
+            extra_context['provider_name'] = profile.get_provider_name(provider)
+            extra_context['profile_image'] = profile.get_avatar_url(provider)
+            extra_context['profile_username'] = profile.get_name(provider)
+            extra_context['profile_gender'] = profile.get_gender(provider)
+            extra_context['profile_url'] = profile.get_social_url(provider)
+            extra_context['profile_email'] = profile.get_email(provider)
+            social_accounts.append(extra_context)
+                
     ## Make the context and render     
-    context = {}
+    context = {'profile': profile, 'social_accounts': social_accounts}
     template = 'dashboard/profile-private.html'
     
     return render(request, template, context)
@@ -56,11 +115,12 @@ def public_profile(request, user_id):
     '''
     Public profile of the user
     '''
-    
+    print "DBG:: public profile"
     user = get_object_or_404(User, pk=user_id)
+    profile = UserProfile.objects.get(user=user) or None #FIXME: giving error when entry does not exist
     
     ## Make the context and render     
-    context = {}
+    context = {'profile': profile}
     template = 'dashboard/profile-public.html'
     
     return render(request, template, context)

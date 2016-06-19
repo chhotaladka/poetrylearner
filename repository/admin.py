327f27cee90a28ed63f0d8855c8d787e4c9fc400
chhotaladka <chhotaladka02@gmail.com>
2016-06-19 10:38:08 +0530

    body/description truncated

--- repository/admin.py
+++ repository/admin.py
@@ -1,69 +1,98 @@
 from django.contrib import admin
+from django.template.defaultfilters import truncatewords
 from repository.models import *
 
 # Register your models here.
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'same_as',
+    list_display = ('name', 'info', 'same_as',
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'description']
     list_filter = ['date_added', 'date_modified']
     
+    def info(self, obj):
+        return truncatewords(obj.description, 10)    
+    info.short_description = 'info'
+    
 
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'has_map', 'address',
+    list_display = ('name', 'info', 'has_map', 'address',
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'description', 'address']
     list_filter = ['date_added', 'date_modified']
     
+    def info(self, obj):
+        return truncatewords(obj.description, 10)    
+    info.short_description = 'info'    
+    
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'manufacturer',
+    list_display = ('name', 'info', 'manufacturer',
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'description']
     list_filter = ['date_added', 'date_modified']
+    
+    def info(self, obj):
+        return truncatewords(obj.description, 10)    
+    info.short_description = 'info'    
     
 
 class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'type',
+    list_display = ('name', 'info', 'type',
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'description']
     list_filter = ['date_added', 'date_modified']
+    
+    def info(self, obj):
+        return truncatewords(obj.description, 10)    
+    info.short_description = 'info'    
     
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 
+    list_display = ('name', 'info', 
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'description']
     list_filter = ['date_added', 'date_modified']
     
+    def info(self, obj):
+        return truncatewords(obj.description, 10)    
+    info.short_description = 'info'    
+    
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'isbn', 'creator', 'publisher',
+    list_display = ('name', 'info', 'isbn', 'creator', 'publisher',
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'description', 'isbn']
     list_filter = ['date_added', 'date_modified']
     
+    def info(self, obj):
+        return truncatewords(obj.description, 10)    
+    info.short_description = 'info'    
+    
 
 class PoetryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'creator', 'body', 'date_published', 
+    list_display = ('name', 'creator', 'info', 'date_published', 
                     'added_by', 'date_added', 
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'body']
     list_filter = ['date_added', 'date_modified']
+    
+    def info(self, obj):
+        return truncatewords(obj.body, 10)    
+    info.short_description = 'body'    
     
 
 class SnippetAdmin(admin.ModelAdmin):
@@ -72,7 +101,7 @@ class SnippetAdmin(admin.ModelAdmin):
                     'modified_by', 'date_modified')
     
     search_fields = ['name', 'body']
-    list_filter = ['date_added', 'date_modified']
+    list_filter = ['date_added', 'date_modified']  
     
 
 class ReferenceAdmin(admin.ModelAdmin):

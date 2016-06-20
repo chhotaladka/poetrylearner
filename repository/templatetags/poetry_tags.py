eeb2231ef79dbd99834b336396c7917c33c01e42
chhotaladka <chhotaladka02@gmail.com>
2016-06-20 21:10:31 +0530

    new template tag added to poetry_tags; poetry count added to person_sm

--- repository/templatetags/poetry_tags.py
+++ repository/templatetags/poetry_tags.py
@@ -6,18 +6,21 @@ from repository.models import Poetry
 
 register = template.Library()
 
+# Flags
+_PUBLISHED = 1
+_UNPUBLISHED = 2
+
 class RecentPoetryNode(Node):
     """
     Render the recent poetry list
     """
 
-    def __init__(self, count=5, varname=None, creator=None, language=None, tag=None, published=False):
+    def __init__(self, count=5, varname=None, creator=None, language=None, tag=None):
         self.count = count
         self.varname = varname
-        self.creator = template.Variable(creator)
+        self.creator = template.Variable(creator) if creator else None
         self.language = language
-        self.tag = tag
-        self.published = published              
+        self.tag = tag              
 
     @classmethod
     def handle_token(cls, parser, token):
@@ -86,21 +89,7 @@ class RecentPoetryNode(Node):
                        count=count, 
                        varname = tokens[5],
                        tag = tokens[3]
-                    )
-                
-        elif tokens[2] == 'published':
-            correct_syntax = "'%s [count] published as [varname]'" % tokens[0]
-            if len(tokens) < 5:
-                raise template.TemplateSyntaxError("Number of arguments are less. Correct syntax is " + correct_syntax)
-            
-            if tokens[3] != 'as':
-                raise template.TemplateSyntaxError("Third argument must be 'as'. Correct syntax is " + correct_syntax)
-
-            return cls(
-                       count=count, 
-                       varname = tokens[4],
-                       published = True
-                    )
+                    )                
         else:
             raise template.TemplateSyntaxError, "Wrong syntax."
         
@@ -141,7 +130,7 @@ class SimilarPoetryNode(Node):
     def __init__(self, count=5, varname=None, poetry=None):
         self.count = count
         self.varname = varname
-        self.poetry = template.Variable(poetry)
+        self.poetry = template.Variable(poetry) if poetry else None
 
     @classmethod
     def handle_token(cls, parser, token):
@@ -193,6 +182,135 @@ class SimilarPoetryNode(Node):
         return ''
 
 
+class PoetryCountNode(Node):
+    """
+    Render the poetry count
+    """
+
+    def __init__(self, varname=None, creator=None, language=None, tag=None, published=False):
+        self.varname = varname
+        self.creator = template.Variable(creator) if creator else None
+        self.language = language
+        self.tag = tag
+        self.published = published             
+
+    @classmethod
+    def handle_token(cls, parser, token):
+        """
+        Class method to parse and return a Node.
+        """
+        tokens = token.split_contents()        
+        
+        if len(tokens) < 3:
+            raise template.TemplateSyntaxError, "%s tag takes at least two arguments" % tokens[0]
+
+        # Check the 1st argument
+        if tokens[1] == 'as':
+            correct_syntax = "'%s as [varname]'" % tokens[0]
+
+            return cls(
+                       varname = tokens[2]
+                    )
+            
+        elif tokens[1] == 'by':            
+            correct_syntax = "'%s by [Person object] as [varname]'" % tokens[0]
+            if len(tokens) < 5:
+                raise template.TemplateSyntaxError, "Number of arguments are less. Correct syntax is " + correct_syntax
+            
+            if tokens[3] != 'as':
+                raise template.TemplateSyntaxError, "Third argument must be 'as'. Correct syntax is " + correct_syntax
+
+            return cls(
+                       varname = tokens[4],
+                       creator = tokens[2]
+                    )
+    
+        elif tokens[1] == 'in':
+            correct_syntax = "'%s in [language] as [varname]'" % tokens[0]
+            if len(tokens) < 5:
+                raise template.TemplateSyntaxError, "Number of arguments are less. Correct syntax is " + correct_syntax
+            
+            if tokens[3] != 'as':
+                raise template.TemplateSyntaxError, "Third argument must be 'as'. Correct syntax is " + correct_syntax
+
+            return cls( 
+                       varname = tokens[4],
+                       language = tokens[2]
+                    )
+                    
+        elif tokens[1] == 'has':
+            correct_syntax = "'%s has [tag] as [varname]'" % tokens[0]
+            if len(tokens) < 5:
+                raise template.TemplateSyntaxError, "Number of arguments are less. Correct syntax is " + correct_syntax
+            
+            if tokens[3] != 'as':
+                raise template.TemplateSyntaxError, "Third argument must be 'as'. Correct syntax is " + correct_syntax
+  
+            return cls(
+                       varname = tokens[4],
+                       tag = tokens[2]
+                    )
+                
+        elif tokens[1] == 'published':
+            correct_syntax = "'%s published as [varname]'" % tokens[0]
+            if len(tokens) < 4:
+                raise template.TemplateSyntaxError("Number of arguments are less. Correct syntax is " + correct_syntax)
+            
+            if tokens[2] != 'as':
+                raise template.TemplateSyntaxError("Second argument must be 'as'. Correct syntax is " + correct_syntax)
+
+            return cls(
+                       varname = tokens[3],
+                       published = _PUBLISHED
+                    )
+
+        elif tokens[1] == 'unpublished':
+            correct_syntax = "'%s unpublished as [varname]'" % tokens[0]
+            if len(tokens) < 4:
+                raise template.TemplateSyntaxError("Number of arguments are less. Correct syntax is " + correct_syntax)
+            
+            if tokens[2] != 'as':
+                raise template.TemplateSyntaxError("Second argument must be 'as'. Correct syntax is " + correct_syntax)
+
+            return cls(
+                       varname = tokens[3],
+                       published = _UNPUBLISHED
+                    )            
+        else:
+            raise template.TemplateSyntaxError, "Wrong syntax."
+        
+
+    def render(self, context):
+        q_objects = Q()
+        try:                        
+            if self.creator:
+                creator = self.creator.resolve(context)
+                q_objects &= Q(creator__id=creator.id)
+            
+            elif self.language:
+                q_objects &= Q(language=self.language)
+                
+            elif self.tag:
+                q_objects &= Q(keywords__slug=self.tag)
+                
+            elif self.published == _PUBLISHED:
+                q_objects &= Q(date_published__isnull=False)
+            
+            elif self.published == _UNPUBLISHED:
+                q_objects &= Q(date_published__isnull=True)
+                                
+            context[self.varname] = Poetry.objects.filter(q_objects).count()
+        
+        except:
+            print ("Error: Unexpected error:", sys.exc_info()[0])
+            for frame in traceback.extract_tb(sys.exc_info()[2]):
+                fname,lineno,fn,text = frame
+                print ("DBG:: Error in %s on line %d" % (fname, lineno)) 
+            raise template.TemplateSyntaxError, "Something went wrong. Check the queryset to resolve the error"            
+            
+        return ''
+
+
 @register.tag
 def get_recent_poetries(parser, token):
     """
@@ -203,14 +321,12 @@ def get_recent_poetries(parser, token):
         case 2: {% get_recent_poetries [count] by [Author object] as [varname] %}
         case 3: {% get_recent_poetries [count] in [language] as [varname] %}
         case 4: {% get_recent_poetries [count] has [tag] as [varname] %}
-        case 5: {% get_recent_poetries [count] published as [varname] %}
         
     Example usage::
         {% get_recent_poetries 5 as poetry_list %}
         {% get_recent_poetries 5 by creator as poetry_list %}
         {% get_recent_poetries 5 in hi as poetry_list %}
         {% get_recent_poetries 5 has tag1 as poetry_list %}
-        {% get_recent_poetries 5 published as poetry_list %}
     """     
     
     return RecentPoetryNode.handle_token(parser, token)
@@ -229,3 +345,28 @@ def get_similar_poetries(parser, token):
     """     
     
     return SimilarPoetryNode.handle_token(parser, token)
+
+
+@register.tag
+def get_poetry_count(parser, token):
+    """
+    Get count of the poetry
+    
+    Syntax::
+        case 1: {% get_poetry_count as [varname] %}
+        case 2: {% get_poetry_count by [Author object] as [varname] %}
+        case 3: {% get_poetry_count in [language] as [varname] %}
+        case 4: {% get_poetry_count has [tag] as [varname] %}
+        case 5: {% get_poetry_count published as [varname] %}
+        case 5: {% get_poetry_count unpublished as [varname] %}
+        
+    Example usage::
+        {% get_poetry_count as count %}
+        {% get_poetry_count by creator as count %}
+        {% get_poetry_count in hi as count %}
+        {% get_poetry_count has tag1 as count %}
+        {% get_poetry_count published as count %}
+        {% get_poetry_count unpublished as count %}
+    """     
+    
+    return PoetryCountNode.handle_token(parser, token)

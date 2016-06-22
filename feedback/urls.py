a8d6b7384822a55cf9c77a8e88e12f30196a808a
chhotaladka <chhotaladka02@gmail.com>
2016-06-22 20:13:11 +0530

    feedback app urls modified

--- feedback/urls.py
+++ feedback/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^data/(?P<pk>\d+)$', views.feedback_response, name='response'),
-    url(r'^data/?$', views.feedback_list, name='list'),
+    url(r'^(?P<pk>\d+)$', views.feedback_response, name='response'),
+    url(r'^$', views.feedback_list, name='list'),
     
     url(r'^add/(?P<ctype_id>\d+)/(?P<obj_id>\d+)/?$', views.FeedbackCreateView.as_view(), name='add_for_content'),    
     url(r'^add/?$', views.FeedbackCreateView.as_view(), name='add'),

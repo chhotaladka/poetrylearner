722959c07f7a9c99055836bb368b65608933d492
chhotaladka <chhotaladka02@gmail.com>
2016-09-05 22:28:20 +0530

    new common tags added

--- common/templatetags/common_tags.py
+++ common/templatetags/common_tags.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
 from django import template
+from datetime import date, datetime
+from django.utils.timezone import is_aware, utc
+from django.utils.translation import pgettext, ugettext as _, ungettext
 
 register = template.Library()
 
@@ -61,3 +64,103 @@ def has_group(user, groups):
             if bool(user.groups.filter(name__in=group_list)) | user.is_superuser:
                 return True
     return False
+
+
+@register.filter
+def niceday(value):
+    '''
+    For date & time values, compared to current timestamp returns representing string.
+    '''
+    if not isinstance(value, date):  # datetime is a subclass of date
+        return value
+    
+    now = datetime.now(utc if is_aware(value) else None)
+    delta = now - value
+    day_delta = delta.days
+    
+    if day_delta == 0:
+        return "today"
+    elif day_delta == 1:
+        return "yesterday"
+    elif day_delta == -1:
+        return "tomorrow"
+    elif value.year == now.year:
+        return value.strftime("%B %d")
+    else:
+        return value.strftime("%B %d, %Y")
+
+
+# Copied from django.contrib.humanize templatetags `naturaltime`
+@register.filter
+def nicetime(value):
+    """
+    For date and time values shows how many seconds, minutes or hours ago
+    compared to current timestamp returns representing string.
+    """
+    if not isinstance(value, date):  # datetime is a subclass of date
+        return value
+
+    now = datetime.now(utc if is_aware(value) else None)
+    if value < now:
+        delta = now - value
+        if delta.days != 0:
+            if delta.days == 1:
+                return 'yesterday'
+            elif value.year == now.year:
+                return value.strftime("%B %d")
+            else:
+                return value.strftime("%B %d, %Y")
+        
+        elif delta.seconds == 0:
+            return _('now')
+        elif delta.seconds < 60:
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a second ago', '%(count)s seconds ago', delta.seconds
+            ) % {'count': delta.seconds}
+        elif delta.seconds // 60 < 60:
+            count = delta.seconds // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a minute ago', '%(count)s minutes ago', count
+                ) % {'count': count}
+        else:
+            count = delta.seconds // 60 // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'an hour ago', '%(count)s hours ago', count
+                ) % {'count': count}
+    else:
+        delta = value - now
+        if delta.days != 0:
+            if delta.days == 1:
+                return 'tomorrow'
+            elif value.year == now.year:
+                return value.strftime("%B %d")
+            else:
+                return value.strftime("%B %d, %Y")
+        elif delta.seconds == 0:
+            return _('now')
+        elif delta.seconds < 60:
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a second from now', '%(count)s seconds from now', delta.seconds
+            ) % {'count': delta.seconds}
+        elif delta.seconds // 60 < 60:
+            count = delta.seconds // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'a minute from now', '%(count)s minutes from now', count
+            ) % {'count': count}
+        else:
+            count = delta.seconds // 60 // 60
+            return ungettext(
+                # Translators: please keep a non-breaking space (U+00A0)
+                # between count and time unit.
+                'an hour from now', '%(count)s hours from now', count
+            ) % {'count': count}

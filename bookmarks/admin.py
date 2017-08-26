26f1163170027922923723a3d4c3267085cf59d9
chhotaladka <chhotaladka02@gmail.com>
2017-08-26 12:36:59 +0530

    admin view update

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -7,7 +7,14 @@ class BookmarkAdmin(admin.ModelAdmin):
     '''
     @summary: Admin class for the `Bookmark` model.
     '''
-    list_display = ['user', 'content_object', 'date_added',]
+    list_display = ['user', 'item', 'date_added',]
     list_filter = ['date_added']
+
+    def item(self, obj):
+        return '<a href="%s">%s</a>' % (obj.get_content_url(), obj.content_object)
+    
+    item.short_description = 'Bookmark Item'
+    item.allow_tags = True
+
 
 admin.site.register(Bookmark, BookmarkAdmin)
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+UserAdmin.list_display = ('first_name', 'last_name', 'email', 'last_login', 'date_joined', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)

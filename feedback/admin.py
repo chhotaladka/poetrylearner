5523a990c1e7ac8da2cf92d2e82521f4c892f881
chhotaladka <chhotaladka02@gmail.com>
2021-05-14 17:14:37 +0530

    feedback: fix admin view

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.template.defaultfilters import truncatewords
-from django.utils.translation import ugettext_lazy as _
+from django.template.defaultfilters import truncatechars
+from django.utils.safestring import mark_safe
 
 from .models import Feedback
 
@@ -10,17 +10,23 @@ class FeedbackAdmin(admin.ModelAdmin):
     @summary: Admin class for the `Feedback` model.
     '''
     
-    list_display = ['text_excerpt', 'user_email', 'url', 'date_added',]
+    list_display = ['text_excerpt', 'user_email', 'content', 'page', 'date_added',]
     
     list_filter = ['date_added',]
     #Giving error: date_hierarchy = 'date_added'
     search_fields = ['added_by__email', 'email', 'url', 'text', ]
 
     def text_excerpt(self, obj):
-        return truncatewords(obj.text, 10)
+        return truncatechars(obj.text, 80)
     
     text_excerpt.short_description = _('Text excerpt')
 
+    def content(self, obj):
+        return mark_safe(f'<a href="{obj.get_content_url()}">{truncatechars(obj.content_object, 40)}</a>')
+
+    def page(self, obj):
+        return mark_safe(f'<a href="{obj.get_page_url()}">link</a>')
+    
     def user_email(self, obj):
         if obj.added_by:
             return obj.added_by.email
